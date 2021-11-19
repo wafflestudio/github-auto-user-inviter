3ffe78f4208b642c04778eaeafa5282710deb691
gina0605 <jina0605@gmail.com>
2021-11-20 07:25:23 +0900

    Change /repository design

--- pages/repository/index.tsx
+++ pages/repository/index.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 
+import { Button, Card, CardActions, CardContent, styled, TextField } from '@mui/material';
 import { useFormik } from 'formik';
 
 import { requester } from '../../apis/requester';
@@ -7,33 +8,83 @@ import { requester } from '../../apis/requester';
 // username: 콤마로 분리
 // permission: select로 변경 필요
 type Form = {
-  token: string;
+  'personal access token': string;
   owner: string;
   permission: 'triage';
   repository: string;
-  username: string;
+  'target usernames': string;
 };
 
 const placeholders: { [key in keyof Form]: string } = {
-  token: 'github personal access token',
+  'personal access token': 'github personal access token',
   owner: 'owner',
   permission: '권한',
   repository: 'repository name',
-  username: ', (콤마) 로 구분된 공백 없는 리스트여야 합니다. 예) woohm402,ars-ki-00,...',
+  'target usernames': ', (콤마) 로 구분된 공백 없는 리스트여야 합니다. 예) woohm402,ars-ki-00,...',
 };
 
+const IDListWrapper = styled('ul')({
+  overflowY: 'auto',
+  maxHeight: 'calc(100% - 40px)',
+  padding: 8,
+  borderRadius: 4,
+  border: '1px solid #ddd',
+});
+
+const FormCard = styled(Card)({
+  position: 'fixed',
+  left: '5vw',
+  width: '50vw',
+  top: 'calc(50% - 300px)',
+  bottom: 'calc(50% - 300px)',
+  padding: 30,
+});
+
+const IDListSucceedCard = styled(Card)({
+  position: 'fixed',
+  right: '5vw',
+  width: '35vw',
+  top: 'calc(50% - 300px)',
+  bottom: 'calc(50% + 20px)',
+  padding: 30,
+});
+
+const IDListFailedCard = styled(Card)({
+  position: 'fixed',
+  right: '5vw',
+  width: '35vw',
+  top: 'calc(50% + 20px)',
+  bottom: 'calc(50% - 300px)',
+  padding: 30,
+});
+
+const IDItem = styled('li')({ listStyleType: 'none' });
+
+const Input = styled(TextField)(() => ({
+  width: '100%',
+  marginBottom: 20,
+}));
+
 const Repository: React.FC = () => {
+  const [succeedList, setSucceedList] = useState<string[]>([]);
   const [failedList, setFailedList] = useState<string[]>([]);
 
-  const { values, handleSubmit, handleChange } = useFormik<Form>({
-    initialValues: { token: '', owner: '', repository: '', username: '', permission: 'triage' },
+  const { values, submitForm, handleChange, setFieldValue } = useFormik<Form>({
+    initialValues: {
+      'personal access token': '',
+      owner: '',
+      permission: 'triage',
+      repository: '',
+      'target usernames': '',
+    },
     onSubmit: async (values) => {
-      const usernames = values.username.split(',');
+      const usernames = values['target usernames'].split(',');
 
       for (const item of usernames) {
         try {
-          await inviteUser(values.owner, values.repository, item, values.permission, values.token);
+          await inviteUser(values.owner, values.repository, item, values.permission, values['personal access token']);
           console.log(`[succeed] ${item}`);
+          setSucceedList((list) => [...list, item]);
         } catch (err) {
           console.log(`[failed ] ${item}`);
           setFailedList((list) => [...list, item]);
@@ -54,39 +105,63 @@ const Repository: React.FC = () => {
     );
   };
 
-  const failedListWithComma = failedList.length >= 1 && failedList.reduce((acc, cur) => acc + ',' + cur);
-
   return (
-    <form onSubmit={handleSubmit}>
-      {['token', 'owner', 'repository', 'permission', 'username'].map((item, i) => (
-        <div key={i}>
-          <span style={{ width: 200, display: 'inline-block', textAlign: 'center', margin: 10 }}>{item}</span>
-          <textarea
-            style={{ width: 600 }}
-            value={values[item as keyof Form]}
-            name={item}
-            onChange={handleChange}
-            placeholder={placeholders[item as keyof Form]}
-          />
-        </div>
-      ))}
-      <br />
-      <br />
-      <br />
-      <div>
-        실패 아이디 목록{' '}
-        <button type={'button'} onClick={() => setFailedList([])}>
-          목록 날리기
-        </button>
-        {failedList.map((item, i) => (
-          <p key={i}>{item}</p>
-        ))}
-      </div>
-      <button type={'submit'}>초대</button>
-      <button type={'button'} onClick={() => navigator.clipboard.writeText(failedListWithComma || '')}>
-        실패 목록 클립보드로 복사
-      </button>
-    </form>
+    <>
+      <FormCard>
+        <CardContent>
+          {Object.entries(placeholders).map(([key, placeholder], i) => (
+            <Input
+              type={key === 'personal access token' ? 'password' : 'text'}
+              autoComplete={'off'}
+              key={i}
+              label={
+                key === 'target usernames' ? `${key} (${values[key as keyof Form].split(',').filter((t) => t).length}명)` : key
+              }
+              value={values[key as keyof Form]}
+              name={key}
+              onChange={(e) => {
+                if (key === 'target usernames') {
+                  setFieldValue('target usernames', e.target.value.replace(/\s/g, ''));
+                } else {
+                  handleChange(e);
+                }
+              }}
+              placeholder={placeholder}
+            />
+          ))}
+        </CardContent>
+        <CardActions>
+          <Button type={'submit'} onClick={submitForm}>
+            초대
+          </Button>
+        </CardActions>
+      </FormCard>
+      <IDListSucceedCard>
+        <span>성공 아이디 목록</span>
+        <IDListWrapper>
+          {succeedList.map((item, i) => (
+            <IDItem key={i}>{item}</IDItem>
+          ))}
+        </IDListWrapper>
+      </IDListSucceedCard>
+      <IDListFailedCard>
+        <span>실패 아이디 목록</span>
+        <Button onClick={() => navigator.clipboard.writeText(failedList.join(','))}>복사</Button>
+        <Button
+          onClick={() => {
+            setFieldValue('target usernames', failedList.join(','));
+          }}
+        >
+          입력
+        </Button>
+        <Button onClick={() => setFailedList([])}>목록 날리기</Button>
+        <IDListWrapper>
+          {failedList.map((item, i) => (
+            <IDItem key={i}>{item}</IDItem>
+          ))}
+        </IDListWrapper>
+      </IDListFailedCard>
+    </>
   );
 };
 
